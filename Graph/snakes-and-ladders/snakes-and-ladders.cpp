0ab84a75b01f32d6b06b514ee7498432920ed88e
Dipesh <74106686+Dipesh123-ux@users.noreply.github.com>
2023-07-04 10:03:51 +0530

    Time: 317 ms (5.05%) | Memory: 77.2 MB (5.11%) - LeetSync

---
+++ Graph/snakes-and-ladders/snakes-and-ladders.cpp
@@ -0,0 +1,78 @@
+class Solution {
+
+    int bfs(int src , int dest,map<int,list<int>> l){
+        map<int,int> dist;
+        queue<int> q; 
+
+        for(auto pair : l){
+            int node = pair.first;
+            dist[node] = -1;
+        }
+
+        q.push(src);
+        dist[src] = 1;
+
+        while(!q.empty()){
+            int node = q.front();
+              q.pop();
+
+           for(int i : l[node])
+            {
+                if(dist[i] == -1){
+                    q.push(i);
+                    dist[i]= dist[node] + 1;
+                }
+            }
+        }
+
+        return dist[dest];
+    }
+ public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        map<int,list<int>> l;
+        int n = board.size();
+        vector<int> newboard(n*n+1,0);
+        int d = true;
+        int k = 1;
+
+        for(int i = n -1; i >= 0; i--){
+            if(d){
+                for(int j = 0; j < n; j++){
+                    newboard[k++] = board[i][j];
+                }
+            }
+            else{
+                for(int j = n-1; j >= 0; j--){
+                    newboard[k++] = board[i][j];
+                }
+            }
+            d = !d;
+        }
+
+        for(int i = 0; i <= n*n; i++){
+            for(int j = 1; j <= 6; j++){
+                int m = i + j;
+                
+                if(m <= n*n){
+                    if(newboard[m] == -1){
+                        newboard[m] = m;
+                    }
+                    l[i].push_back(newboard[m]);
+                }
+            }
+        }
+
+   
+        
+        l[n*n].push_back(n*n);
+
+        
+        int ans = bfs(1,n*n,l);
+
+        return (ans == -1 ? -1 : ans-1);
+        
+
+        
+
+    }
+};
