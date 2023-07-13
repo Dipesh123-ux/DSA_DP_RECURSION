fc02853154b20296099c3b757a1c529012568bab
Dipesh <74106686+Dipesh123-ux@users.noreply.github.com>
2023-07-13 06:58:36 +0530

    Time: 7 ms (88.35%) | Memory: 13.9 MB (64.02%) - LeetSync

--- Graph/flood-fill/flood-fill.cpp
+++ Graph/flood-fill/flood-fill.cpp
@@ -1,32 +1,31 @@
 class Solution {
-public:
 
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,-1,0,1};
+ int dx[4] = {1,-1,0,0};
+ int dy[4] = {0, 0,-1,1};
 
-void fill(vector<vector<int>>&image,int r,int c,int i,int j,int color,int prev){
-    if(i < 0 || j < 0 || i >= r || j >= c) {
+void solve(vector<vector<int>>& image,int r,int c, int sr, int sc, int color,int init){
+    if(sr < 0 || sc < 0 || sr >= r || sc >= c){
         return;
     }
 
-
-
-    if(image[i][j] != prev || image[i][j] == color){
+    if(image[sr][sc] == color || image[sr][sc] != init){
         return;
     }
+    
+    image[sr][sc] = color;
 
-    image[i][j] = color;
-   
-    for(int k = 0; k < 4; k++){
-        fill(image,r,c,i+dx[k],j+dy[k],color,prev);
-    } 
+    for(int i = 0; i < 4; i++){
+        solve(image,r,c,sr+dx[i],sc+dy[i],color,init);
+    }
+
 }
 
-
+public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int r = image.size();
         int c = image[0].size();
-        fill(image,r,c,sr,sc,color,image[sr][sc]);
+        int init = image[sr][sc];
+        solve(image,r,c,sr,sc,color,init);
         return image;
     }
 };
