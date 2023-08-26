fd2fdc3a0e300cce7527c57307b32f4e3fccf41c
Dipesh <74106686+Dipesh123-ux@users.noreply.github.com>
2023-08-26 12:39:31 +0530

    Time: 64 ms (20.07%) | Memory: 42.3 MB (15.56%) - LeetSync

---
+++ Graph/27-remove-element/remove-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let c = 0;
+    for(let i = 0; i < nums.length;i++){
+        if(nums[i] != val){
+            nums[c++] = nums[i];
+        }
+    }
+
+    return c;
+};
