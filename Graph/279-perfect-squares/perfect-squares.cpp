a9387adda5d3c929e2ec8b5997115ac05fee743a
Dipesh <74106686+Dipesh123-ux@users.noreply.github.com>
2024-02-08 23:08:44 +0530

    Time: 46 ms (86.64%) | Memory: 10.4 MB (38.33%) - LeetSync

--- Graph/279-perfect-squares/perfect-squares.cpp
+++ Graph/279-perfect-squares/perfect-squares.cpp
@@ -4,10 +4,10 @@ public:
       vector<int> dp(n+1,0);
       for(int i = 1; i <= n; i++){
           int ans = INT_MAX;
-          for(int j = 1; j <= i; j++){
-              if(j*j <= i){
+          for(int j = 1; j*j <= i; j++){
+         
                   ans = min(ans,1+dp[i-j*j]);
-              }
+              
           }
           dp[i] = ans;
       }
